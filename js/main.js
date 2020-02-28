90127670d1e5a85466d6e69ef6bc97c05eb74a7e
muhammadweper <qayumov0196@gmail.com>
2020-02-28 11:01:18 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function smile() {
+// document.getElementById('btn1').src='http://localhost:63342/lump%20switch%20of%20%E2%80%94%20%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F/images/happy.png'
+//     document.getElementById('btn').src='http://localhost:63342/lump%20switch%20of%20%E2%80%94%20%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F/images/neutral.png'
+
+ //   console.log(document.getElementById('rasm').src);
+
+}
